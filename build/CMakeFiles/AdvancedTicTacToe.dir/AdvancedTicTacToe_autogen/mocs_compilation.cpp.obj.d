0c9a068062e72c20e5fe4a0670eae263401a0bc8
Your Name <you@example.com>
2025-06-19 22:25:13 +0300

    AI Test

---
+++ build/CMakeFiles/AdvancedTicTacToe.dir/AdvancedTicTacToe_autogen/mocs_compilation.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/AdvancedTicTacToe.dir/AdvancedTicTacToe_autogen/mocs_compilation.cpp.obj: \
+ C:\Users\omary\My-Github\TicTacToe\build\AdvancedTicTacToe_autogen\mocs_compilation.cpp
---
+++ build/CMakeFiles/TicTacToeCore.dir/TicTacToeCore_autogen/mocs_compilation.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/TicTacToeCore.dir/TicTacToeCore_autogen/mocs_compilation.cpp.obj: \
+ C:\Users\omary\My-Github\TicTacToe\build\TicTacToeCore_autogen\mocs_compilation.cpp
