c1c6cf3c22b36caca11bcef30e96596b72b146ed
Your Name <you@example.com>
2025-06-20 04:15:56 +0300

    Updated CMakeLists

--- build/CMakeFiles/TicTacToeCore.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TicTacToeCore.
-# This may be replaced when dependencies are built.
---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,8 @@
+set(__QT_DEPLOY_TARGET_TicTacToeCore_FILE C:/Users/omary/My-Github/TicTacToe/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/libTicTacToeCore.a)
+set(__QT_DEPLOY_TARGET_TicTacToeCore_TYPE STATIC_LIBRARY)
+set(__QT_DEPLOY_TARGET_TicTacToe_FILE C:/Users/omary/My-Github/TicTacToe/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/TicTacToe.exe)
+set(__QT_DEPLOY_TARGET_TicTacToe_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TicTacToe_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
+set(__QT_DEPLOY_TARGET_RunTests_FILE C:/Users/omary/My-Github/TicTacToe/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/RunTests.exe)
+set(__QT_DEPLOY_TARGET_RunTests_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_RunTests_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll;C:/msys64/mingw64/bin/libgtest_main.dll;C:/msys64/mingw64/bin/libgtest.dll)
